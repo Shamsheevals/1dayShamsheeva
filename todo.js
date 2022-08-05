53dcca15acf4117140eb072c11a878e9b8dce775
Олеся Шамшеева <shamsheevals@gmail.com>
2022-08-05 17:00:45 +0300

    todo js

--- todo.js
+++ todo.js
@@ -1,35 +1,48 @@
-const input = document.querySelector("#input");
-const button = document.querySelector("#button");
-const list = document.querySelector("#list");
-const result = document.querySelector("#result");
+const $input = document.getElementById("input");
+const $button = document.getElementById("button");
+const $list = document.getElementById("list");
+const $result = document.getElementById("result");
+
 let i = 0;
 
+function createOnEnter(e) {
+  if (e.keyCode == 13)
+    document.getElementById('addTasks').click();
+}
+window.addEventListener("keyup", createOnEnter);
 
 
-button.addEventListener("click", (e) => {
-    if (input.value === "") return;
-    createDeleteElements(input.value);
-    input.value = "";
+$button.addEventListener("click", (e) => {
+  const valueTasks = $input.value.trim()
+  if (valueTasks === "") return;
+  createElement($input.value);
+  $input.value = "";
 });
 
-const createDeleteElements = (value) => {
-    i++;
-    const li = document.createElement("li");
-    li.className = "li";
-    li.textContent = value;
-    const button = document.createElement("button");
-    button.className = "button";
-    button.textContent = "удалить";
-    li.appendChild(button);
-    button.addEventListener("click", (e) => {
-        list.removeChild(li);
-        i--;
-        result.textContent = i;
-    });
-    li.addEventListener("click", (e) => {
-        li.classList.toggle("li-active");
-    });
-    result.textContent = i;
-    list.appendChild(li);
+const createElement = (value) => {
+  const liTodo = document.createElement("li");
+  liTodo.className = "li";
+  liTodo.textContent = value;
+  i++; //create task (li)
+
+  const deleteButton = document.createElement("button");
+  deleteButton.className = "button";
+  deleteButton.textContent = "удалить";
+  liTodo.append(deleteButton); // create 'delete' button
+
+  deleteButton.addEventListener("click", (e) => {
+    $list.removeChild(li);
+    i--;
+    $result.textContent = i;
+  });
+  const completedButton = document.createElement("button")
+  completedButton.textContent = "Завершено"
+  liTodo.prepend(completedButton);
+  completedButton.addEventListener("click", (e) => {
+    liTodo.classList.toggle("li-active");
+
+  });
+  $result.textContent = i;
+  $list.appendChild(liTodo);
 };
 
