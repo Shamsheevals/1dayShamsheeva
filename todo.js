818851a2b396914cf44ff4f6f653c670f69f86ce
Олеся Шамшеева <shamsheevals@gmail.com>
2022-08-08 09:38:34 +0300

    todo

--- todo.js
+++ todo.js
@@ -2,60 +2,86 @@ const $input = document.getElementById("input");
 const $button = document.getElementById("button");
 const $list = document.getElementById("list");
 const $result = document.getElementById("result");
-
-let i = 0;
+const tasksArr = [];
+let counter = 0;
 
 function createOnEnter(e) {
   if (e.key === "Enter")
     document.getElementById('addTasks').click();
 }
+
 $input.addEventListener("keydown", createOnEnter);
 
 
 $button.addEventListener("click", (e) => {
-  const valueTasks = $input.value.trim()
-  if (!valueTasks) {
-    return;
-  }
-  createElement($input.value);
-  $input.value = "";
+  createTodo($input.value);
 });
 
-const createElement = (value) => {
-  const $todoItem = document.createElement("li");
-  $todoItem.className = "li";
-  $todoItem.textContent = value;
-  i++; // create task (li)
+// const createElement = (value) => {
+//   const $todoItem = document.createElement("li");
+//   $todoItem.className = "li";
+//   $todoItem.textContent = value;
+//   i++; 
 
-  const $deleteButton = document.createElement("button");
-  $deleteButton.className = "button";
-  $deleteButton.textContent = "удалить";
-  $todoItem.append($deleteButton); // create 'delete' button
+//   const $deleteButton = document.createElement("button");
+//   $deleteButton.className = "button";
+//   $deleteButton.textContent = "удалить";
+//   $todoItem.append($deleteButton); // create 'delete' button
 
-  $deleteButton.addEventListener("click", () => {
-    $todoItem.remove();
-    i--;
-    $result.textContent = i;
-  });
-  const $completedButton = document.createElement("button")
-  $completedButton.textContent = "Завершено"
-  $todoItem.prepend($completedButton);
-  $completedButton.addEventListener("click", (e) => {
-    $todoItem.classList.toggle("li-active");
-  });
-  $result.textContent = i;
-  $list.appendChild($todoItem);
-};
+//   $deleteButton.addEventListener("click", () => {
+//     $todoItem.remove();
+//     i--;
+//     $result.textContent = i;
+//   });
+//   const $completedButton = document.createElement("button")
+//   $completedButton.textContent = "Завершено"
+//   $todoItem.prepend($completedButton);
+//   $completedButton.addEventListener("click", (e) => {
+//     $todoItem.classList.toggle("li-active");
+//   });
+//   $result.textContent = i;
+//   $list.appendChild($todoItem);
+// };
+
+
 
 const createTodo = (title) => {
-  //
-  render();
+  const valueTask = title.trim()
+  if (!valueTask) {
+    return;
+  }
+  const task = {
+    text: valueTask,
+    isActive: true,
+    id: Date.now(),
+  }
+  tasksArr.push(task);
+  console.log(tasksArr)
+  $input.value = "";
+  render(tasksArr);
 };
 
-const render = () => {
+const completeTodo=(id)=>{
+  tasksArr.map( ( el ) => el.id === id && {...el,isActive:!el.isActive});
+}
+const removeTodo=(id)=>{
+  tasksArr.filter(el=>el.id!==id)
+}
+const render = (tasksArr) => {
   $list.innerHTML = "";
-
-  //
+  tasksArr.map(task => {
+    console.log(task)
+    const li = document.createElement("li")
+    li.className = "li";
+    li.innerHTML = `
+    <button onclick=${completeTodo(task.id)}>Завершить </button>
+     ${task.text} 
+     <button onclick=${removeTodo(task.i)}> Удалить </button>
+    `;
+    $list.appendChild(li)
+    $result.textContent = `${ tasksArr.length }`;
+  });
 };
 
 // Add Listeners
+ 
