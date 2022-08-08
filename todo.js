e456b9c26f3aaa1e11f431db37ec438598731eaa
Олеся Шамшеева <109203276+Shamsheevals@users.noreply.github.com>
2022-08-08 12:57:36 +0300

    Add files via upload

--- todo.js
+++ todo.js
@@ -2,86 +2,77 @@ const $input = document.getElementById("input");
 const $button = document.getElementById("button");
 const $list = document.getElementById("list");
 const $result = document.getElementById("result");
-const tasksArr = [];
+let tasksArr = [];
 let counter = 0;
 
 function createOnEnter(e) {
-  if (e.key === "Enter")
-    document.getElementById('addTasks').click();
+  if (e.key === "Enter") document.getElementById("addTasks").click();
 }
 
 $input.addEventListener("keydown", createOnEnter);
-
 
 $button.addEventListener("click", (e) => {
   createTodo($input.value);
 });
 
-// const createElement = (value) => {
-//   const $todoItem = document.createElement("li");
-//   $todoItem.className = "li";
-//   $todoItem.textContent = value;
-//   i++; 
-
-//   const $deleteButton = document.createElement("button");
-//   $deleteButton.className = "button";
-//   $deleteButton.textContent = "удалить";
-//   $todoItem.append($deleteButton); // create 'delete' button
-
-//   $deleteButton.addEventListener("click", () => {
-//     $todoItem.remove();
-//     i--;
-//     $result.textContent = i;
-//   });
-//   const $completedButton = document.createElement("button")
-//   $completedButton.textContent = "Завершено"
-//   $todoItem.prepend($completedButton);
-//   $completedButton.addEventListener("click", (e) => {
-//     $todoItem.classList.toggle("li-active");
-//   });
-//   $result.textContent = i;
-//   $list.appendChild($todoItem);
-// };
-
-
-
 const createTodo = (title) => {
-  const valueTask = title.trim()
+  const valueTask = title.trim();
   if (!valueTask) {
     return;
   }
   const task = {
     text: valueTask,
-    isActive: true,
+    isCheked: false,
     id: Date.now(),
-  }
+  };
   tasksArr.push(task);
-  console.log(tasksArr)
+  console.log(tasksArr);
   $input.value = "";
-  render(tasksArr);
+ return render();
 };
 
-const completeTodo=(id)=>{
-  tasksArr.map( ( el ) => el.id === id && {...el,isActive:!el.isActive});
-}
-const removeTodo=(id)=>{
-  tasksArr.filter(el=>el.id!==id)
-}
-const render = (tasksArr) => {
-  $list.innerHTML = "";
-  tasksArr.map(task => {
-    console.log(task)
-    const li = document.createElement("li")
-    li.className = "li";
-    li.innerHTML = `
-    <button onclick=${completeTodo(task.id)}>Завершить </button>
-     ${task.text} 
-     <button onclick=${removeTodo(task.i)}> Удалить </button>
-    `;
-    $list.appendChild(li)
-    $result.textContent = `${ tasksArr.length }`;
+const completeTodo = (id) => {
+  tasksArr = tasksArr.map((todoItem) => {
+    if (todoItem.id !== id) {
+      return todoItem;
+    }
+    return {
+      ...todoItem,
+      isCheked: !todoItem.isCheked,
+    };
   });
+  render();
 };
 
-// Add Listeners
- 
+const removeTodo = (id) => {
+  tasksArr = tasksArr.filter((el) => el.id !== id);
+  render();
+};
+
+const render = () => {
+  $list.innerHTML = "";
+  tasksArr.map((task) => {
+    console.log(task);
+    const li = document.createElement("li");
+    li.className = "li";
+    if (task.isCheked) {
+      li.classList.add("li-active");
+    }
+    li.innerHTML = `
+   <button onclick="completeTodo(${task.id})">Завершено</button>
+    ${task.text}
+    <button onclick="removeTodo(${task.id})"> Удалить </button>`;
+    $list.appendChild(li);
+    $result.textContent = `${tasksArr.length}`;
+  });
+  return;
+};
+
+
+
+
+
+
+
+
+
